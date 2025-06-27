e93afa02a64dac4d0baf4e53d111156b169b4b49
mehedi <mehedisun@outlook.com>
2025-06-27 08:03:57 +0600

    last update

--- solutions/AppointmentService/Appointment_Contracts/obj/Release/net10.0/Appointment_Contracts.AssemblyInfo.cs
+++ solutions/AppointmentService/Appointment_Contracts/obj/Release/net10.0/Appointment_Contracts.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Appointment_Contracts")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493894f8d089316e4ea5f98157585c4775f1059b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Appointment_Contracts")]
